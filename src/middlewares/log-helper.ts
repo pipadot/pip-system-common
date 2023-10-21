03556660270f16bcd32b9725cbf0c6236622cf8c
Hieu Quang <hieu.nq701@gmail.com>
2023-10-21 23:15:29 +0700

    Updates

--- src/middlewares/log-helper.ts
+++ src/middlewares/log-helper.ts
@@ -3,7 +3,7 @@ import moment from 'moment';
 export const logInfo = (name: string, input?: any, output?: any) => {
   console.log(
     `${moment().format(
-      'HH:MM DD/MM/YYYY'
+      'HH:mm DD/MM/YYYY'
     )} - [${name}] || INFO || Input: ${JSON.stringify(
       input
     )} || Output: ${JSON.stringify(output)} \n`
@@ -12,7 +12,7 @@ export const logInfo = (name: string, input?: any, output?: any) => {
 export const logEvent = (name: string, input?: any, output?: any) => {
   console.log(
     `${moment().format(
-      'HH:MM DD/MM/YYYY'
+      'HH:mm DD/MM/YYYY'
     )} - [${name}] || EVENT || Input: ${JSON.stringify(
       input
     )} || Output: ${JSON.stringify(output)} \n`
@@ -21,7 +21,7 @@ export const logEvent = (name: string, input?: any, output?: any) => {
 export const logError = (name: string, input?: any, output?: any) => {
   console.log(
     `${moment().format(
-      'HH:MM DD/MM/YYYY'
+      'HH:mm DD/MM/YYYY'
     )} - [${name}] || ERROR || Input: ${JSON.stringify(
       input
     )}|| Output: ${JSON.stringify(output)} \n`
